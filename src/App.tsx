990e04b443b719e4467bbf531bdad13634be194d
Alexandre Czechowicz <alexandrecz@Alexandres-MacBook-Air.local>
2022-09-05 14:30:20 -0300

    :art: just a fine tune

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />∏
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
